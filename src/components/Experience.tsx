5d0abf39ac6ade978cefd7a99a9a652fe4af18fa
asfafasa <giftofgodashish@gmail.com>
2023-12-18 11:59:49 +0530

    added box mdel

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,14 +1,16 @@
 import { OrbitControls, ScrollControls } from "@react-three/drei";
 import Office from "./Office.jsx";
+import Animate from "./Animate.jsx";
+
 import { Overlay } from "./Overlay.js";
 const Experience = () => {
   return (
     <>
-      <ambientLight intensity={1}>
-        <OrbitControls enableZoom={false} enabled={false} />
-        <ScrollControls pages={3} damping={0.25}>
-          <Overlay />
-          <Office />
+      <ambientLight intensity={2}>
+        <OrbitControls />
+        <ScrollControls>
+          {/* <Overlay /> */}
+          <Animate />
         </ScrollControls>
       </ambientLight>
     </>
